b7cdbf6ab0b17c13d6499bc7742228eb5b369b96
jim <spammailhere99@yahoo.com>
2018-09-29 16:44:13 -0500

    Initial functional commit

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 set -e
 
 sudo docker build \
-    -t ens \
+    -t ethnetstat \
     .
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,12 @@ set -ex
 
 cd /opt/eth-net-intelligence-api
 sed -i "s/RPC_HOST_IP/${RPC_HOST_IP}/g" ./app.json
-exec pm2-docker start app.json
+echo "exec pm2-docker start app.json"
+exec pm2-docker start app.json &
+
+sleep 1
 
 cd /opt/eth-netstats
 export WS_SECRET=internal-to-this-container
+echo "npm start"
 npm start
